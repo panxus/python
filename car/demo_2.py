e8fd10216edd97fb40e73b2412118699b68697ad
panxu <1058149613@qq.com>
2017-07-24 22:40:46 +0800

    v2.0 2017年7月24日22:40:44

--- car/demo_2.py
+++ car/demo_2.py
@@ -11,43 +11,43 @@ class parseStr(object):
         self.carTypeName = []
         self.carColor = []
         self.carInnerColor = []
+        self.carZhiDaoPrice = []
+        self.carcityTime = []
 
-    def getSeriesId(self):
-        # 车辆id
-        return re.search(r'.*var\s+specIDs\s*=\s*(.*?);',self.js,re.S).group(1)
-
-    def getConfig(self):
-        # 车辆配置 词典dict
-        return re.search(r'.*var\s+config\s*=\s*(.*?);',self.js,re.S).group(1)
-
-    def getColor(self):
+    def getSetting(self):
+        # 车辆id  词典dict
+        seriesIds = re.search(r'.*var\s+specIDs\s*=\s*(.*?);',self.js,re.S).group(1)
+        # 车辆配置
+        configs = re.search(r'.*var\s+config\s*=\s*(.*?);',self.js,re.S).group(1)
         # 外观颜色
-        return re.search(r'.*var\s+color\s*=\s*(.*?);',self.js,re.S).group(1)
-
-    def getInnerColor(self):
+        colors = re.search(r'.*var\s+color\s*=\s*(.*?);',self.js,re.S).group(1)
         # 内饰颜色
-        return re.search(r'.*var\s+innerColor\s*=\s*(.*?);',self.js,re.S).group(1)
+        innerColor = re.search(r'.*var\s+innerColor\s*=\s*(.*?);',self.js,re.S).group(1)
+
+        return seriesIds,configs,colors,innerColor
 
     def strChangeDict(self,str):
         # 数据转换
         s = str.replace('null',"'null'")
         return eval(s)
 
+
     def getResult(self):
-        configStr = self.getConfig()
-        configDict = self.strChangeDict(configStr)
-
-        colorStr = self.getColor()
-        colorDict = self.strChangeDict(colorStr)
-
-        iColorStr = self.getInnerColor()
-        iColorDIct = self.strChangeDict(iColorStr)
-
         try:
-            carSeriesId = self.getSeriesId()
+            carSeriesId,configStr,colorStr,iColorStr= self.getSetting()
+            configDict = self.strChangeDict(configStr)
+            colorDict = self.strChangeDict(colorStr)
+            iColorDIct = self.strChangeDict(iColorStr)
+            #车系名称
             carTypeName = configDict['result']['paramtypeitems'][0]['paramitems'][0]['valueitems']
+            #指导价
+            zhiDaoPrice = configDict['result']['paramtypeitems'][0]['paramitems'][1]['valueitems']
+            # 上市时间
+            cityTime = configDict['result']['paramtypeitems'][0]['paramitems'][4]['valueitems']
+            # 外观颜色
             carColor = colorDict['result']['specitems'][0]['coloritems']
-            innerColor = iColorDIct['result']['specitems'][0]['coloritems']
+            # 内饰颜色
+            innerColor = iColorDIct['result']['specitems']
         except Exception,e:
             print 'field error:',e
         else:
@@ -55,6 +55,8 @@ class parseStr(object):
             self.carTypeName = carTypeName
             self.carColor = carColor
             self.carInnerColor = innerColor
+            self.carZhiDaoPrice = zhiDaoPrice
+            self.carcityTime = cityTime
 
 
 
@@ -66,20 +68,22 @@ class parseStr(object):
 
 if __name__ == '__main__':
     ######### ①. #########
-    # javascript = file('test_2.html')
-    # jsContent = javascript.read()
+    javascript = file('test_1.html')
+    jsContent = javascript.read()
 
     ######### ②. #########
     # url = 'http://car.autohome.com.cn/config/series/923.html'
-    url = 'http://car.autohome.com.cn/config/series/3170.html'
-    s = requests.get(url)
-    con = s.text
-    jsContent = Selector(text=con).xpath('/html/body/script[7]').extract()[0]
-    print jsContent
+    # url = 'http://car.autohome.com.cn/config/series/650.html'
+    # s = requests.get(url)
+    # con = s.text
+    # jsContent = Selector(text=con).xpath('/html/body/script[7]').extract()[0]
+    # print jsContent
 
-    # s = parseStr(jsContent)
-    # s.getResult()
-    # print s.carSeriesId
-    # print s.carTypeName
-    # print s.carColor
-    # print s.carInnerColor
+    s = parseStr(jsContent)
+    s.getResult()
+    print s.carSeriesId
+    print s.carTypeName
+    print s.carColor
+    print s.carInnerColor
+    print s.carZhiDaoPrice
+    print s.carcityTime
--- car/trans.py
+++ car/trans.py
@@ -1,17 +1,184 @@
 #coding=utf-8
 
 cTran = {
-    "<span class='hs_kw2_configpl'></span>":'奥迪',
-    "<span class='hs_kw3_configpl'></span>":'尚',
-    "<span class='hs_kw4_configpl'></span>":'',
-    "<span class='hs_kw6_configpl'></span>":'',
-    "<span class='hs_kw7_configpl'></span>":'',
-    "<span class='hs_kw8_configpl'></span>":'',
-    "<span class='hs_kw10_configpl'></span>":'',
-    "<span class='hs_kw11_configpl'></span>":'',
-    "<span class='hs_kw12_configpl'></span>":'',
-    "<span class='hs_kw13_configpl'></span>":'',
-    "<span class='hs_kw14_configpl'></span>":'',
 
+"<span class='hs_kw89_configpl'></span>": "全尺寸",
+
+"<span class='hs_kw88_configpl'></span>": "备胎",
+
+"<span class='hs_kw87_configpl'></span>": "后轮胎",
+
+"<span class='hs_kw86_configpl'></span>": "规格",
+
+"<span class='hs_kw85_configpl'></span>": "前轮胎",
+
+"<span class='hs_kw84_configpl'></span>": "驻车",
+
+"<span class='hs_kw83_configpl'></span>": "后制动器",
+
+"<span class='hs_kw82_configpl'></span>": "盘式",
+
+"<span class='hs_kw81_configpl'></span>": "风",
+
+"<span class='hs_kw80_configpl'></span>": "通风",
+
+"<span class='hs_kw79_configpl'></span>": "前制动器",
+
+"<span class='hs_kw78_configpl'></span>": "承载式",
+
+"<span class='hs_kw77_configpl'></span>": "液压",
+
+"<span class='hs_kw76_configpl'></span>": "机械",
+
+"<span class='hs_kw75_configpl'></span>": "助力",
+
+"<span class='hs_kw74_configpl'></span>": "扭力梁",
+
+"<span class='hs_kw73_configpl'></span>": "后悬架",
+
+"<span class='hs_kw72_configpl'></span>": "独立",
+
+"<span class='hs_kw71_configpl'></span>": "连杆",
+
+"<span class='hs_kw70_configpl'></span>": "多连杆",
+
+"<span class='hs_kw69_configpl'></span>": "悬架",
+
+"<span class='hs_kw68_configpl'></span>": "前悬架",
+
+"<span class='hs_kw67_configpl'></span>": "前驱",
+
+"<span class='hs_kw66_configpl'></span>": "前置",
+
+"<span class='hs_kw65_configpl'></span>": "欧",
+
+"<span class='hs_kw64_configpl'></span>": "标准",
+
+"<span class='hs_kw63_configpl'></span>": "环保",
+
+"<span class='hs_kw62_configpl'></span>": "铁",
+
+"<span class='hs_kw61_configpl'></span>": "缸体",
+
+"<span class='hs_kw60_configpl'></span>": "铝",
+
+"<span class='hs_kw59_configpl'></span>": "缸盖",
+
+"<span class='hs_kw58_configpl'></span>": "电喷",
+
+"<span class='hs_kw57_configpl'></span>": "多点",
+
+"<span class='hs_kw56_configpl'></span>": "供油",
+
+"<span class='hs_kw55_configpl'></span>": "燃油",
+
+"<span class='hs_kw54_configpl'></span>": "扭矩",
+
+"<span class='hs_kw53_configpl'></span>": "转速",
+
+"<span class='hs_kw52_configpl'></span>": "功率",
+
+"<span class='hs_kw51_configpl'></span>": "最大",
+
+"<span class='hs_kw50_configpl'></span>": "行程",
+
+"<span class='hs_kw49_configpl'></span>": "缸径",
+
+"<span class='hs_kw48_configpl'></span>": "机构",
+
+"<span class='hs_kw47_configpl'></span>": "配气",
+
+"<span class='hs_kw46_configpl'></span>": "压缩比",
+
+"<span class='hs_kw45_configpl'></span>": "气门数",
+
+"<span class='hs_kw44_configpl'></span>": "排列",
+
+"<span class='hs_kw43_configpl'></span>": "气缸",
+
+"<span class='hs_kw42_configpl'></span>": "增压",
+
+"<span class='hs_kw41_configpl'></span>": "涡轮",
+
+"<span class='hs_kw40_configpl'></span>": "吸气",
+
+"<span class='hs_kw39_configpl'></span>": "自然",
+
+"<span class='hs_kw38_configpl'></span>": "进气",
+
+"<span class='hs_kw37_configpl'></span>": "排量",
+
+"<span class='hs_kw36_configpl'></span>": "号",
+
+"<span class='hs_kw35_configpl'></span>": "容积",
+
+"<span class='hs_kw34_configpl'></span>": "油箱",
+
+"<span class='hs_kw33_configpl'></span>": "车门数",
+
+"<span class='hs_kw32_configpl'></span>": "质量",
+
+"<span class='hs_kw31_configpl'></span>": "整备",
+
+"<span class='hs_kw30_configpl'></span>": "后轮距",
+
+"<span class='hs_kw29_configpl'></span>": "前轮距",
+
+"<span class='hs_kw28_configpl'></span>": "前",
+
+"<span class='hs_kw27_configpl'></span>": "轴距",
+
+"<span class='hs_kw26_configpl'></span>": "高度",
+
+"<span class='hs_kw25_configpl'></span>": "宽度",
+
+"<span class='hs_kw24_configpl'></span>": "长度",
+
+"<span class='hs_kw23_configpl'></span>": "年不限公里",
+"<span class='hs_kw22_configpl'></span>": "质保",
+
+"<span class='hs_kw21_configpl'></span>": "离地间隙",
+
+"<span class='hs_kw20_configpl'></span>": "综合",
+
+"<span class='hs_kw19_configpl'></span>": "油耗",
+
+"<span class='hs_kw18_configpl'></span>": "实测",
+
+"<span class='hs_kw17_configpl'></span>": "大型车",
+
+"<span class='hs_kw16_configpl'></span>": "中大型车",
+
+"<span class='hs_kw15_configpl'></span>": "大众",
+
+"<span class='hs_kw14_configpl'></span>": "一汽",
+
+"<span class='hs_kw13_configpl'></span>": "万",
+
+"<span class='hs_kw12_configpl'></span>": "元",
+
+"<span class='hs_kw11_configpl'></span>": "价",
+
+"<span class='hs_kw10_configpl'></span>": "导",
+
+"<span class='hs_kw9_configpl'></span>": "指",
+
+"<span class='hs_kw8_configpl'></span>": "商",
+
+"<span class='hs_kw7_configpl'></span>" :"版",
+
+"<span class='hs_kw6_configpl'></span>": "华",
+
+"<span class='hs_kw5_configpl'></span>": "豪",
+
+"<span class='hs_kw4_configpl'></span>": "适",
+
+"<span class='hs_kw3_configpl'></span>": "舒",
+
+"<span class='hs_kw2_configpl'></span>": "奥迪",
+
+"<span class='hs_kw1_configpl'></span>": "名称",
+
+"<span class='hs_kw0_configpl'></span>": "车型",
 
 }
