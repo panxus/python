1d878623d88a293426dfa81a7a08bb4f4cdbd77e
panxu94 <1058149613@qq.com>
2017-07-27 18:11:08 +0800

    demo file

---
+++ car02/demo_3.py
@@ -0,0 +1,112 @@
+# coding=utf-8
+
+
+
+s = {"returncode": 0, "message": "成功", "result": {
+    "branditems": [{"id": 117, "name": "AC Schnitzer", "bfirstletter": "A"},
+                   {"id": 276, "name": "ALPINA", "bfirstletter": "A"},
+                   {"id": 34, "name": "阿尔法·罗密欧", "bfirstletter": "A"},
+                   {"id": 35, "name": "阿斯顿·马丁", "bfirstletter": "A"}, {"id": 221, "name": "安凯客车", "bfirstletter": "A"},
+                   {"id": 33, "name": "奥迪", "bfirstletter": "A"}, {"id": 140, "name": "巴博斯", "bfirstletter": "B"},
+                   {"id": 120, "name": "宝骏", "bfirstletter": "B"}, {"id": 15, "name": "宝马", "bfirstletter": "B"},
+                   {"id": 231, "name": "宝沃", "bfirstletter": "B"}, {"id": 40, "name": "保时捷", "bfirstletter": "B"},
+                   {"id": 27, "name": "北京", "bfirstletter": "B"}, {"id": 203, "name": "北汽幻速", "bfirstletter": "B"},
+                   {"id": 173, "name": "北汽绅宝", "bfirstletter": "B"}, {"id": 143, "name": "北汽威旺", "bfirstletter": "B"},
+                   {"id": 208, "name": "北汽新能源", "bfirstletter": "B"}, {"id": 154, "name": "北汽制造", "bfirstletter": "B"},
+                   {"id": 36, "name": "奔驰", "bfirstletter": "B"}, {"id": 95, "name": "奔腾", "bfirstletter": "B"},
+                   {"id": 14, "name": "本田", "bfirstletter": "B"}, {"id": 271, "name": "比速汽车", "bfirstletter": "B"},
+                   {"id": 75, "name": "比亚迪", "bfirstletter": "B"}, {"id": 13, "name": "标致", "bfirstletter": "B"},
+                   {"id": 38, "name": "别克", "bfirstletter": "B"}, {"id": 39, "name": "宾利", "bfirstletter": "B"},
+                   {"id": 37, "name": "布加迪", "bfirstletter": "B"}, {"id": 79, "name": "昌河", "bfirstletter": "C"},
+                   {"id": 76, "name": "长安", "bfirstletter": "C"}, {"id": 163, "name": "长安欧尚", "bfirstletter": "C"},
+                   {"id": 294, "name": "长安轻型车", "bfirstletter": "C"}, {"id": 77, "name": "长城", "bfirstletter": "C"},
+                   {"id": 196, "name": "成功汽车", "bfirstletter": "C"}, {"id": 169, "name": "DS", "bfirstletter": "D"},
+                   {"id": 92, "name": "大发", "bfirstletter": "D"}, {"id": 1, "name": "大众", "bfirstletter": "D"},
+                   {"id": 41, "name": "道奇", "bfirstletter": "D"}, {"id": 32, "name": "东风", "bfirstletter": "D"},
+                   {"id": 187, "name": "东风风度", "bfirstletter": "D"}, {"id": 259, "name": "东风风光", "bfirstletter": "D"},
+                   {"id": 113, "name": "东风风神", "bfirstletter": "D"}, {"id": 165, "name": "东风风行", "bfirstletter": "D"},
+                   {"id": 142, "name": "东风小康", "bfirstletter": "D"}, {"id": 81, "name": "东南", "bfirstletter": "D"},
+                   {"id": 42, "name": "法拉利", "bfirstletter": "F"}, {"id": 11, "name": "菲亚特", "bfirstletter": "F"},
+                   {"id": 3, "name": "丰田", "bfirstletter": "F"}, {"id": 141, "name": "福迪", "bfirstletter": "F"},
+                   {"id": 197, "name": "福汽启腾", "bfirstletter": "F"}, {"id": 8, "name": "福特", "bfirstletter": "F"},
+                   {"id": 96, "name": "福田", "bfirstletter": "F"}, {"id": 282, "name": "福田乘用车", "bfirstletter": "F"},
+                   {"id": 112, "name": "GMC", "bfirstletter": "G"}, {"id": 152, "name": "观致", "bfirstletter": "G"},
+                   {"id": 116, "name": "光冈", "bfirstletter": "G"}, {"id": 82, "name": "广汽传祺", "bfirstletter": "G"},
+                   {"id": 108, "name": "广汽吉奥", "bfirstletter": "G"}, {"id": 24, "name": "哈飞", "bfirstletter": "H"},
+                   {"id": 181, "name": "哈弗", "bfirstletter": "H"}, {"id": 150, "name": "海格", "bfirstletter": "H"},
+                   {"id": 86, "name": "海马", "bfirstletter": "H"}, {"id": 267, "name": "汉腾汽车", "bfirstletter": "H"},
+                   {"id": 43, "name": "悍马", "bfirstletter": "H"}, {"id": 164, "name": "恒天", "bfirstletter": "H"},
+                   {"id": 91, "name": "红旗", "bfirstletter": "H"}, {"id": 245, "name": "华凯", "bfirstletter": "H"},
+                   {"id": 237, "name": "华利", "bfirstletter": "H"}, {"id": 85, "name": "华普", "bfirstletter": "H"},
+                   {"id": 220, "name": "华颂", "bfirstletter": "H"}, {"id": 87, "name": "华泰", "bfirstletter": "H"},
+                   {"id": 260, "name": "华泰新能源", "bfirstletter": "H"}, {"id": 97, "name": "黄海", "bfirstletter": "H"},
+                   {"id": 188, "name": "Icona", "bfirstletter": "I"}, {"id": 46, "name": "Jeep", "bfirstletter": "J"},
+                   {"id": 25, "name": "吉利汽车", "bfirstletter": "J"}, {"id": 84, "name": "江淮", "bfirstletter": "J"},
+                   {"id": 119, "name": "江铃", "bfirstletter": "J"}, {"id": 210, "name": "江铃集团轻汽", "bfirstletter": "J"},
+                   {"id": 270, "name": "江铃集团新能源", "bfirstletter": "J"}, {"id": 44, "name": "捷豹", "bfirstletter": "J"},
+                   {"id": 83, "name": "金杯", "bfirstletter": "J"}, {"id": 145, "name": "金龙", "bfirstletter": "J"},
+                   {"id": 175, "name": "金旅", "bfirstletter": "J"}, {"id": 151, "name": "九龙", "bfirstletter": "J"},
+                   {"id": 109, "name": "KTM", "bfirstletter": "K"}, {"id": 156, "name": "卡尔森", "bfirstletter": "K"},
+                   {"id": 224, "name": "卡升", "bfirstletter": "K"}, {"id": 199, "name": "卡威", "bfirstletter": "K"},
+                   {"id": 101, "name": "开瑞", "bfirstletter": "K"}, {"id": 47, "name": "凯迪拉克", "bfirstletter": "K"},
+                   {"id": 214, "name": "凯翼", "bfirstletter": "K"}, {"id": 219, "name": "康迪全球鹰", "bfirstletter": "K"},
+                   {"id": 100, "name": "科尼赛克", "bfirstletter": "K"}, {"id": 9, "name": "克莱斯勒", "bfirstletter": "K"},
+                   {"id": 241, "name": "LOCAL MOTORS", "bfirstletter": "L"},
+                   {"id": 118, "name": "Lorinser", "bfirstletter": "L"},
+                   {"id": 48, "name": "兰博基尼", "bfirstletter": "L"}, {"id": 54, "name": "劳斯莱斯", "bfirstletter": "L"},
+                   {"id": 52, "name": "雷克萨斯", "bfirstletter": "L"}, {"id": 10, "name": "雷诺", "bfirstletter": "L"},
+                   {"id": 124, "name": "理念", "bfirstletter": "L"}, {"id": 80, "name": "力帆汽车", "bfirstletter": "L"},
+                   {"id": 89, "name": "莲花汽车", "bfirstletter": "L"}, {"id": 78, "name": "猎豹汽车", "bfirstletter": "L"},
+                   {"id": 51, "name": "林肯", "bfirstletter": "L"}, {"id": 53, "name": "铃木", "bfirstletter": "L"},
+                   {"id": 204, "name": "陆地方舟", "bfirstletter": "L"}, {"id": 88, "name": "陆风", "bfirstletter": "L"},
+                   {"id": 49, "name": "路虎", "bfirstletter": "L"}, {"id": 50, "name": "路特斯", "bfirstletter": "L"},
+                   {"id": 56, "name": "MINI", "bfirstletter": "M"}, {"id": 58, "name": "马自达", "bfirstletter": "M"},
+                   {"id": 57, "name": "玛莎拉蒂", "bfirstletter": "M"}, {"id": 55, "name": "迈巴赫", "bfirstletter": "M"},
+                   {"id": 129, "name": "迈凯伦", "bfirstletter": "M"}, {"id": 20, "name": "名爵", "bfirstletter": "M"},
+                   {"id": 168, "name": "摩根", "bfirstletter": "M"}, {"id": 130, "name": "纳智捷", "bfirstletter": "N"},
+                   {"id": 213, "name": "南京金龙", "bfirstletter": "N"}, {"id": 60, "name": "讴歌", "bfirstletter": "O"},
+                   {"id": 59, "name": "欧宝", "bfirstletter": "O"}, {"id": 146, "name": "欧朗", "bfirstletter": "O"},
+                   {"id": 61, "name": "帕加尼", "bfirstletter": "P"}, {"id": 26, "name": "奇瑞", "bfirstletter": "Q"},
+                   {"id": 122, "name": "启辰", "bfirstletter": "Q"}, {"id": 62, "name": "起亚", "bfirstletter": "Q"},
+                   {"id": 235, "name": "前途", "bfirstletter": "Q"}, {"id": 63, "name": "日产", "bfirstletter": "R"},
+                   {"id": 19, "name": "荣威", "bfirstletter": "R"}, {"id": 174, "name": "如虎", "bfirstletter": "R"},
+                   {"id": 296, "name": "瑞驰新能源", "bfirstletter": "R"}, {"id": 103, "name": "瑞麒", "bfirstletter": "R"},
+                   {"id": 45, "name": "smart", "bfirstletter": "S"},
+                   {"id": 269, "name": "SWM斯威汽车", "bfirstletter": "S"}, {"id": 64, "name": "萨博", "bfirstletter": "S"},
+                   {"id": 205, "name": "赛麟", "bfirstletter": "S"}, {"id": 68, "name": "三菱", "bfirstletter": "S"},
+                   {"id": 149, "name": "陕汽通家", "bfirstletter": "S"}, {"id": 155, "name": "上汽大通", "bfirstletter": "S"},
+                   {"id": 66, "name": "世爵", "bfirstletter": "S"}, {"id": 90, "name": "双环", "bfirstletter": "S"},
+                   {"id": 69, "name": "双龙", "bfirstletter": "S"}, {"id": 162, "name": "思铭", "bfirstletter": "S"},
+                   {"id": 65, "name": "斯巴鲁", "bfirstletter": "S"}, {"id": 238, "name": "斯达泰克", "bfirstletter": "S"},
+                   {"id": 67, "name": "斯柯达", "bfirstletter": "S"}, {"id": 202, "name": "泰卡特", "bfirstletter": "T"},
+                   {"id": 133, "name": "特斯拉", "bfirstletter": "T"}, {"id": 161, "name": "腾势", "bfirstletter": "T"},
+                   {"id": 283, "name": "WEY", "bfirstletter": "W"}, {"id": 102, "name": "威麟", "bfirstletter": "W"},
+                   {"id": 99, "name": "威兹曼", "bfirstletter": "W"}, {"id": 192, "name": "潍柴英致", "bfirstletter": "W"},
+                   {"id": 284, "name": "蔚来", "bfirstletter": "W"}, {"id": 70, "name": "沃尔沃", "bfirstletter": "W"},
+                   {"id": 114, "name": "五菱汽车", "bfirstletter": "W"}, {"id": 167, "name": "五十铃", "bfirstletter": "W"},
+                   {"id": 98, "name": "西雅特", "bfirstletter": "X"}, {"id": 12, "name": "现代", "bfirstletter": "X"},
+                   {"id": 185, "name": "新凯", "bfirstletter": "X"}, {"id": 71, "name": "雪佛兰", "bfirstletter": "X"},
+                   {"id": 72, "name": "雪铁龙", "bfirstletter": "X"}, {"id": 111, "name": "野马汽车", "bfirstletter": "Y"},
+                   {"id": 110, "name": "一汽", "bfirstletter": "Y"}, {"id": 144, "name": "依维柯", "bfirstletter": "Y"},
+                   {"id": 73, "name": "英菲尼迪", "bfirstletter": "Y"}, {"id": 93, "name": "永源", "bfirstletter": "Y"},
+                   {"id": 298, "name": "宇通客车", "bfirstletter": "Y"}, {"id": 263, "name": "驭胜", "bfirstletter": "Y"},
+                   {"id": 182, "name": "之诺", "bfirstletter": "Z"}, {"id": 206, "name": "知豆", "bfirstletter": "Z"},
+                   {"id": 22, "name": "中华", "bfirstletter": "Z"}, {"id": 74, "name": "中兴", "bfirstletter": "Z"},
+                   {"id": 94, "name": "众泰", "bfirstletter": "Z"}]}}
+
+
+from handledb import exec_sql,insert_manydata
+kwargs={'user':'root','passwd':'root','db':'tp_blog','host':'localhost','port':3307, 'charset':'utf8'}
+
+try:
+    if s['returncode'] == 0 and s['message'] == '成功':
+        last = s['result']['branditems']
+        itm = [(l['bfirstletter'],l['id'],l['name']) for l in last]
+        sql = ('''insert into home_brand (firstletter,brand_id,brand_name) values (%s,%s,%s)''')
+        data = tuple(itm)
+        insert_manydata(sql,data,**kwargs)
+
+    else:
+        print 'response error'
+except Exception,e:
+    print 'field error',e
