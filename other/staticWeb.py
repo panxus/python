fbd0f34be1c8a44eba0ddddb823b24ac2dcaf472
panxu <1058149613@qq.com>
2017-08-18 22:49:51 +0800

    更新为python3

--- other/staticWeb.py
+++ other/staticWeb.py
@@ -1,10 +1,11 @@
 #coding=utf-8
 import requests
-import urlparse
+from urllib import parse
 import os
 import random
 import re
 from bs4 import BeautifulSoup
+import codecs
 class ParseWeb(object):
     def __init__(self,url):
         # self.rcompile = re.compile(r'url.*?\'(.*?fonts.*?)\'.*?\)')
@@ -16,18 +17,18 @@ class ParseWeb(object):
         # 1.验证url
         code,html = self._verify()
         if code:
-            self.box = html
+            self.box = html.decode('utf-8')
             # 2.建立目录
             dirc = self._makeDir()
             # 3.解析页面
             if dirc:
                 self._parse(dirc)
         else:
-            print '页面解析失败'
+            print('页面解析失败')
 
 
     def _makeDir(self):
-        urlSe = urlparse.urlparse(self.url)
+        urlSe = parse.urlparse(self.url)
         dir_f = urlSe.netloc+'_1'
         flag = True
         while flag and os.path.isdir(dir_f):
@@ -63,8 +64,8 @@ class ParseWeb(object):
             self._downResource(hi,dirc)
             if self._isCss(hi):
                 #解析css样式里可能存在的字体样式
-                hi =  urlparse.urljoin(self.url,hi)
-                s = requests.get(hi).content
+                hi =  parse.urljoin(self.url,hi)
+                s = requests.get(hi).content.decode('utf-8')
                 index1 = hi.rfind('/')
                 hi = hi[:index1]
                 index2 = hi.rfind('/')
@@ -85,32 +86,35 @@ class ParseWeb(object):
         #替换百度统计
         self.box = self.scompile.sub('""',self.box)
 
-        with open(dirc+'/index.html','wb+') as f:
-            f.write(self.box)
+        try:
+            with open(dirc+'/index.html','wb+') as f:
+                f.write(self.box.encode('utf-8'))
             f.close()
+        except Exception as e:
+            print('页面写入失败',e)
 
     # 下载资源
     # url 资源
     # dirc 主文件夹名
     def _downResource(self,url,dirc):
         url = self._purl(url)
-        old_url = urlparse.urljoin(self.url,url)
+        old_url = parse.urljoin(self.url,url)
         diro = self._getDirByUrl(url)
         fname = url.split('/')[-1]
         real_dir = dirc+'/'+diro+'/'+fname
         sreq = requests.get(old_url)
         try:
             if sreq.status_code == 200:
-                with open(real_dir,'wb') as f:
+                with codecs.open(real_dir,'wb') as f:
                     f.write(sreq.content)
                     f.close()
-                print '文件 %s 下载到 %s 成功.' % (old_url,real_dir)
+                print('文件 %s 下载到 %s 成功.' % (old_url,real_dir))
                 newUrl = './'+diro+'/'+fname
                 self.box = self.box.replace(url,newUrl)
             else:
-                print '文件 %s 下载到 %s 失败.' % (old_url,real_dir)
-        except Exception,e:
-            print e
+                print('文件 %s 下载到 %s 失败.' % (old_url,real_dir))
+        except Exception as e:
+            print(e)
 
 
     def _purl(self,url):
@@ -150,6 +154,6 @@ class ParseWeb(object):
             return False,con.content
 
 if __name__ == '__main__':
-    start_url = 'http://cuiqingcai.com/'
+    start_url = 'http://coladesign.cn/'
     pObj = ParseWeb(start_url)
 
